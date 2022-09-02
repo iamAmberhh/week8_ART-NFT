e298a17bad763ce84e5557e080ca5dbcbf67b8e1
Amber <melody19941231@gmail.com>
2022-09-02 19:35:25 +0800

    second commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,23 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  $('.search').click(function(e){
+    e.preventDefault();
+    if(jQuery(window).width() < 768){
+      $('h1').addClass('d-none');
+      $('.navbar-toggler').addClass('d-none');
+      $('.search-bar').removeClass('d-none');
+      $('.search-back').removeClass('d-none');
+      $('.search-form').removeClass('ms-auto');
+      $('.search-form').addClass('p-1');
+    }
+    
+  });
+  $('.search-back').click(function(e){
+    $('h1').removeClass('d-none');
+    $('.navbar-toggler').removeClass('d-none');
+    $('.search-bar').addClass('d-none');
+    $('.search-back').addClass('d-none');
+    $('.search-form').addClass('ms-auto');
+    $('.search-form').removeClass('p-1');
+  });
 });
+
