b0e9775b3fe2d61da3da069a814da23f2eaf2a00
Amber <melody19941231@gmail.com>
2022-09-10 20:22:34 +0800

    依助教建議修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,26 +1,30 @@
-$(function() {
-  $('.search').click(function(e){
-    e.preventDefault();
-    if(jQuery(window).width() < 768){
-      $('h1').addClass('d-none');
-      $('.navbar-toggler').addClass('d-none');
-      $('.search-bar').removeClass('d-none');
-      $('.search-back').removeClass('d-none');
-      $('.search-form').removeClass('ms-auto');
-      $('.search-form').addClass('p-1');
-    }
-    
-  });
-  $('.search-back').click(function(e){
-    $('h1').removeClass('d-none');
-    $('.navbar-toggler').removeClass('d-none');
-    $('.search-bar').addClass('d-none');
-    $('.search-back').addClass('d-none');
-    $('.search-form').addClass('ms-auto');
-    $('.search-form').removeClass('p-1');
-  });
-  $('.table-triangle').click(function(e){
-    $(this).toggleClass('table-turn');
-  })
-});
+$(function () {
+  // $(window).resize(function () {
+  //   if (jQuery(window).width() > 768) {
+  //       $("h1").removeClass("d-none");
+  //   } 
+  // });
 
+  // $(".search").click(function (e) {
+  //   e.preventDefault();
+  //   if (jQuery(window).width() < 768) {
+  //     $("h1").addClass("d-none");
+  //     $(".navbar-toggler").addClass("d-none");
+  //     $(".search-bar").removeClass("d-none");
+  //     $(".search-back").removeClass("d-none");
+  //     $(".search-form").removeClass("ms-auto");
+  //     $(".search-form").addClass("p-1");
+  //   }
+  // });
+  // $(".search-back").click(function (e) {
+  //   $("h1").removeClass("d-none");
+  //   $(".navbar-toggler").removeClass("d-none");
+  //   $(".search-bar").addClass("d-none");
+  //   $(".search-back").addClass("d-none");
+  //   $(".search-form").addClass("ms-auto");
+  //   $(".search-form").removeClass("p-1");
+  // });
+  $(".table-triangle").click(function (e) {
+    $(this).toggleClass("table-turn");
+  });
+});
